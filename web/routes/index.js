69aad3dcc532269d30f1eac07c5219ecec2c723f
Rohithd <58989625+rohithdubba@users.noreply.github.com>
2025-07-26 11:36:12 -0400

    Update index.js
    test lecture 3

--- web/routes/index.js
+++ web/routes/index.js
@@ -12,7 +12,7 @@ router.get('/ice-flakes', function (req, res, next) {
     .status(201)
     .json({
       resource: 'ice-flakes',
-      count: 1006,
+      count: 1007,
       shape: 'rectangle'
     })
 })
